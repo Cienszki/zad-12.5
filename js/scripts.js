9acbbc78257342aefecdb0e014439cc4ca833219
Cienszki <wilq.wdz@gmail.com>
2018-03-13 14:49:23 +0100

    Add loader and style

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var button = $('.trigger');
 
 function getQuote() {
+	button.prop("disabled", true).html('<i class="fa fa-spinner fa-spin"></i>');
 	$.getJSON(prefix + quoteUrl, createTweet);
 	$.ajaxSetup({ cache: false });
+
+	
 }
 
-function createTweet(input) { // co to jest input?  co to jest klucz?
-	var data = input [0];
+function createTweet(input) {
+	var data = input[0];
+	
 
 	var quoteText = $(data.content).text().trim();
 	var quoteAuthor = data.title;
 
-	if (!quoteAuthor.lenght) {
+	if (!quoteAuthor.length) {
 		quoteAuthor = 'Unknown Author';
 	}
 	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
@@ -27,12 +32,15 @@ function createTweet(input) { // co to jest input?  co to jest klucz?
 		$('.author').text('Author: ' + quoteAuthor);
 		$('.tweet').attr('href', tweet);
 	}
-
+	button.prop("disabled", false).html('Random quote');
 }
 
 $(document).ready(function() {
 	getQuote();
 	$('.trigger').click(function() {
+		
 		getQuote();
+		
+		
 	});
 });
